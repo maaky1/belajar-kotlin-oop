8c7a079adf6ca3acf3c54f83ae94488850ba1727
M Agus F <aagusfikry1@gmail.com>
2024-12-23 16:42:40 +0700

    Learn initializer block

--- src/com/github/maaky1/data/Car.kt
+++ src/com/github/maaky1/data/Car.kt
@@ -1,6 +1,11 @@
 package com.github.maaky1.data
 
 class Car(paramBrand: String, paramYear: Int = 2000) {
+
+    init {
+        println("Car $paramBrand dibuat")
+    }
+
     var brand: String = paramBrand
     var year: Int = paramYear
 }
