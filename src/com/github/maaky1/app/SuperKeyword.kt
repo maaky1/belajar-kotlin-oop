d70fcfa46d3f42e5307b136e9d66ab28e29152f6
M Agus F <aagusfikry1@gmail.com>
2024-12-24 16:12:43 +0700

    Learn super keyword

---
+++ src/com/github/maaky1/app/SuperKeyword.kt
@@ -0,0 +1,10 @@
+package com.github.maaky1.app
+
+import com.github.maaky1.data.Rectangle
+
+fun main() {
+    val rectangle = Rectangle()
+    println(rectangle.corner)
+    println(rectangle.parrentCorner)
+    rectangle.printName()
+}
--- src/com/github/maaky1/data/Shape.kt
+++ src/com/github/maaky1/data/Shape.kt
@@ -2,12 +2,20 @@ package com.github.maaky1.data
 
 open class Shape {
     open val corner: Int = -1
+    open fun printName() {
+        println("This is Shape")
+    }
 }
 
 class Rectangle : Shape() {
     override val corner: Int = 4
+    val parrentCorner: Int = super.corner
+    override fun printName() {
+        println("This is Rectangle")
+        super.printName()
+    }
 }
 
-class Triangle : Shape() {
+open class Triangle : Shape() {
     override val corner: Int = 3
 }
