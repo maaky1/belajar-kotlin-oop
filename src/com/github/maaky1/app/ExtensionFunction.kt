8295f0942745266c77d95feae39c0ef1fef34296
M Agus F <aagusfikry1@gmail.com>
2025-01-13 21:44:58 +0700

    Learn extension function

---
+++ src/com/github/maaky1/app/ExtensionFunction.kt
@@ -0,0 +1,9 @@
+package com.github.maaky1.app
+
+import com.github.maaky1.data.Student
+import com.github.maaky1.data.sayHello
+
+fun main() {
+    val student = Student("Agus", 20)
+    student.sayHello("Budi")
+}
---
+++ src/com/github/maaky1/data/Student.kt
@@ -0,0 +1,9 @@
+package com.github.maaky1.data
+
+class Student(val name: String, val age: Int)
+
+fun Student?.sayHello(name: String) {
+    if (this != null) {
+        println("Hello $name, my name is ${this.name}, and my age is ${this.age}")
+    }
+}
