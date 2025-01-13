32e801818d62ec86d516f246b11d073ceb5417f9
M Agus F <aagusfikry1@gmail.com>
2025-01-13 21:48:18 +0700

    Learn extension properties

--- src/com/github/maaky1/data/Student.kt
+++ src/com/github/maaky1/data/Student.kt
@@ -7,3 +7,6 @@ fun Student?.sayHello(name: String) {
         println("Hello $name, my name is ${this.name}, and my age is ${this.age}")
     }
 }
+
+val Student.gedeinLah: String
+    get() = this.name.uppercase()
